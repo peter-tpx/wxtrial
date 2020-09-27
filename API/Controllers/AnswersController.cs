6ea378851466d08f054254d8675e2c63fd7b487d
peter-tpx <peter.tpx@gmail.com>
2020-09-28 06:32:39 +1000

    test for deployment
    test fn
    
    
    Draft exercise 1

---
+++ API/Controllers/AnswersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AnswersController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<AnswersController> _logger;
+
+        public AnswersController(IConfiguration configuration, ILogger<AnswersController> logger)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("user")]
+        public IActionResult GetUser()
+        {
+            var result = new
+            {
+                name = "test",
+                token = _configuration["Identifiers:token"]
+            };
+
+            return Ok(result);
+
+        }
+    }
+}
